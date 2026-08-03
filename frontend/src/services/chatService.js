64acd445fb92f177ddbf8503066689878562a168
Yassin Mohammed <iamyassin25@gmail.com>
2026-08-03 20:46:55 +0530

    deploying frontend

--- frontend/src/services/chatService.js
+++ frontend/src/services/chatService.js
@@ -1,5 +1,5 @@
-export const API_BASE_URL = "http://127.0.0.1:8000";
-
+export const API_BASE_URL =
+  import.meta.env.VITE_API_BASE_URL;
 
 export const sendChatMessage = async (message) => {
   const response = await fetch(`${API_BASE_URL}/chat`, {
